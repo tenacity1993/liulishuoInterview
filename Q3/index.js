614b596efce48d7aef0df4441d5e74ef6a503d83
tenacity1993 <tenacitychen@gmail.com>
2017-10-02 20:14:43 +0800

    add class and  eventlistener for mask

--- Q3/index.js
+++ Q3/index.js
@@ -1,29 +1,43 @@
 function Toast() {
-    this.init();
+    this.init()
     return this
 }
 Toast.prototype = {
     duration: 3000,
     init: function() {
-        let parentElem = document.getElementById('toast');
-        let mask = document.getElementById('mask');
+        let parentElem = document.getElementById('toast')
+        let mask = document.getElementById('mask')
         if (!parentElem) {
-            parentElem = document.createElement('div');
-            parentElem.id = "toast";
-            document.body.appendChild(parentElem);
+            parentElem = document.createElement('div')
+            parentElem.id = "toast"
+            document.body.appendChild(parentElem)
         }
     },
     show(options) {
         let obj = options || {}
+        let msg = obj.msg || '默认显示内容'
+        let icon = obj.icon || 'triangle'
+        let elemClass = obj.class || 'dialog'
+        let pNode = document.getElementById('toast')
         let mask = document.createElement('div')
+        mask.id = 'mask'
         mask.setAttribute('class', 'mask')
         let elem = document.createElement('div')
         elem.setAttribute('class', 'content')
+        let html = `<div class="${elemClass}"><i class="${icon}"></i><p>${msg}</p></div>`
+        elem.innerHTML = html
         document.getElementById('toast').appendChild(mask)
         document.getElementById('toast').appendChild(elem)
-        let pNode = document.getElementById('toast')
+
+        let maskElem = document.getElementById('mask')
+        maskElem.addEventListener('click', () => {
+            if(pNode.hasChildNodes()) {
+                pNode.parentNode.removeChild(pNode);
+            }
+        }, false)
+
         // setTimeout(() => {
-        //     pNode.parentNode.removeChild(pNode);
+        //     pNode.parentNode.removeChild(pNode)
         // }, obj.duration || this.duration)
     },
     close() {
@@ -33,8 +47,8 @@ Toast.prototype = {
 }
 
 let toast = new Toast()
-toast.show()
-setTimeout(() => {
+toast.show('系统异常')
+/*setTimeout(() => {
     toast.close()
-}, 3000)
+}, 3000)*/
 // toast.close()
