2c0bda85242102264fce670d6b44be9516962131
tenacity1993 <tenacitychen@gmail.com>
2017-10-02 19:35:58 +0800

    add close func

--- Q3/index.js
+++ Q3/index.js
@@ -1,18 +1,40 @@
 function Toast() {
+    this.init();
     return this
 }
 Toast.prototype = {
     duration: 3000,
-    show: function (options) {
+    init: function() {
+        let parentElem = document.getElementById('toast');
+        let mask = document.getElementById('mask');
+        if (!parentElem) {
+            parentElem = document.createElement('div');
+            parentElem.id = "toast";
+            document.body.appendChild(parentElem);
+        }
+    },
+    show(options) {
         let obj = options || {}
         let mask = document.createElement('div')
         mask.setAttribute('class', 'mask')
         let elem = document.createElement('div')
         elem.setAttribute('class', 'content')
-        document.body.appendChild(mask)
-        document.body.appendChild(elem)
+        document.getElementById('toast').appendChild(mask)
+        document.getElementById('toast').appendChild(elem)
+        let pNode = document.getElementById('toast')
+        // setTimeout(() => {
+        //     pNode.parentNode.removeChild(pNode);
+        // }, obj.duration || this.duration)
+    },
+    close() {
+        let pNode = document.getElementById('toast')
+        pNode.parentNode.removeChild(pNode);
     }
 }
 
 let toast = new Toast()
 toast.show()
+setTimeout(() => {
+    toast.close()
+}, 3000)
+// toast.close()
